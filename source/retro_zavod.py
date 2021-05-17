39f3ae6405ef68258ee1e4315947d73bfb6ed50b
Ondřej <foltin@spstrutnov.cz>
2021-05-17 18:02:03 +0200

    +Main Menu
    přidáno hlavní menu s funkčními tlačítky

--- source/retro_zavod.py
+++ source/retro_zavod.py
@@ -1,5 +1,4 @@
 import pygame, sys, random, time
-from time import sleep
 pygame.init()
 
 #název
@@ -20,8 +19,14 @@ pygame.mixer.music.load("soundtrack.mp3")
 pygame.mixer.music.set_volume(0.4)
 
 #načíst obrázky
-fotoauta = pygame.image.load("auto.png")
-fotoauta2 = pygame.image.load("auto2.png")
+fotoauta = pygame.image.load("auto.png") #RED
+fotoauta2 = pygame.image.load("auto2.png") #GREEN
+fotoauta3 = pygame.image.load("auto3.png") #BLUE
+fotoauta4 = pygame.image.load("auto4.png") #YELLOW
+fotoauta5 = pygame.image.load("auto5.png") #BLACK
+fotoauta6 = pygame.image.load("auto6.png") #WHITE
+
+menufoto = pygame.image.load("RetroRacingMENU.png")
 
 trava1 = pygame.image.load("trava.png")
 trava2 = pygame.image.load("trava.png")
@@ -31,6 +36,15 @@ silnice = pygame.image.load("silnicepre.png")
 
 crashfoto = pygame.image.load("crashed.png")
 crashexploze = pygame.image.load("crash.png")
+
+play = pygame.image.load("playbutton.png")
+options = pygame.image.load("optionsbutton.png")
+quit = pygame.image.load("quitbutton.png")
+
+play2 = pygame.image.load("playbutton2.png")
+options2 = pygame.image.load("optionsbutton2.png")
+quit2 = pygame.image.load("quitbutton2.png")
+
 
 #ikona
 pygame.display.set_icon(fotoauta)
@@ -42,13 +56,71 @@ sirka = 270 // 4
 vyska = 460 // 4
 fotoauta = pygame.transform.scale(fotoauta, (sirka, vyska))
 fotoauta2 = pygame.transform.scale(fotoauta2, (sirka, vyska))
+fotoauta3 = pygame.transform.scale(fotoauta3, (sirka, vyska))
+fotoauta4 = pygame.transform.scale(fotoauta4, (sirka, vyska))
+fotoauta5 = pygame.transform.scale(fotoauta5, (sirka, vyska))
+fotoauta6 = pygame.transform.scale(fotoauta6, (sirka, vyska))
+
+#MENU
+def mainmenu():
+    menu = True
+    zvoleno = "start"
+
+    while menu:
+        udalosti = pygame.event.get()
+        for udalost in udalosti:
+            if udalost.type==pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if udalost.type==pygame.KEYDOWN:
+                if udalost.key==pygame.K_UP:
+                    zvoleno="start"
+                elif udalost.key==pygame.K_DOWN:
+                    zvoleno="quit"
+                if udalost.key==pygame.K_RETURN:
+                    if zvoleno=="start":
+                        print("Start")
+                    if zvoleno=="quit":
+                        pygame.quit()
+                        quit()
+        
+        okno.blit(menufoto, (0,0))
+        
+        okno.blit(play, (550, 390))
+        okno.blit(options, (510, 440))
+        okno.blit(quit, (550, 490))
+        
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        
+        
+        if mouse[0] > 555 and mouse[0] < 670 and mouse[1] > 395 and mouse[1] < 435:
+            okno.blit(play2, (550, 390))
+            if click == (True, False, False):
+                gameloop()
+            
+        if mouse[0] > 513 and mouse[0] < 711 and mouse[1] > 443 and mouse[1] < 483:
+            okno.blit(options2, (510, 440))
+           # if click == (True, False, False):
+           # DODĚLAT OPTIONS !!! # Fullscreen etc. #
+            
+        if mouse[0] > 553 and mouse[0] < 662 and mouse[1] > 493 and mouse[1] < 533:
+            okno.blit(quit2, (550, 490))
+            if click == (True, False, False):
+                pygame.quit()
+                sys.exit()
+        
+        
+        pygame.display.update()
 
 def highscore(pocet):
     font = pygame.font.Font("VCR_OSD_MONO.ttf",35)
     text = font.render("Score:"+str(pocet),True,255)
     okno.blit(text,(30,30))
+    pocet = 0
     
 def prekazky(prekx,preky,prekazka):
+    okno.blit(prekazka, (prekx,preky))
     okno.blit(prekazka, (prekx,preky))
     
 #pozadí
@@ -87,21 +159,36 @@ def crash(autoX, autoY):
     sirka_crash = 722 // 6
     vyska_crash = 702 // 6
     pozice_crashX = autoX
+    
+    crashexploze_mensi = pygame.transform.scale(crashexploze, (sirka_crash, vyska_crash))
+    okno.blit(crashexploze_mensi, (pozice_crashX, autoY))    
+  
+    #překážka č.1
     prekazka_startx = random.randrange(400,700-sirka)
     prekazka_starty = -600
     prek_sirka = 50
     prek_vyska = 100
     prek_speed = 10
-    pocet = 0
+ 
+    if autoX >= prekazka_startx and autoX <= prekazka_startx+prek_sirka:
+        pozice_crashX = autoX - 50
         
+    if autoX+sirka >= prekazka_startx and autoX+sirka <= prekazka_startx+prek_sirka:
+        pozice_crashX = autoX + 10
+        
+    #překážka č.2
+    prekazka_startx = random.randrange(100,300)
+    prekazka_starty = -600
+    prek_sirka = 50
+    prek_vyska = 100
+    prek_speed = 10
+    
     if autoX >= prekazka_startx and autoX <= prekazka_startx+prek_sirka:
         pozice_crashX = autoX - 50
         
     if autoX+sirka >= prekazka_startx and autoX+sirka <= prekazka_startx+prek_sirka:
         pozice_crashX = autoX + 10
          
-    crashexploze_mensi = pygame.transform.scale(crashexploze, (sirka_crash, vyska_crash))
-    okno.blit(crashexploze_mensi, (pozice_crashX, autoY))
     
 #    if autoX <= 100:
 #        pozice_crashX = autoX - 50
@@ -139,13 +226,22 @@ def gameloop():
     silnice_y2 = -600
     silnice_speed = 8
     silnice_speed_change = 0
-    pygame.mixer.music.play(-1)
+    #pygame.mixer.music.play(-1)
+    pocet = 0
+    
+    #překážka č.1
     prekazka_startx = random.randrange(400,700-sirka)
     prekazka_starty = -600
     prek_sirka = 50
     prek_vyska = 100
     prek_speed = 10
-    pocet = 0
+    
+    #překážka č.2
+    prekazka_startx = random.randrange(100,300)
+    prekazka_starty = -600
+    prek_sirka = 50
+    prek_vyska = 100
+    prek_speed = 10
     
     while spusteno:
         stisknuto = pygame.key.get_pressed()
@@ -159,25 +255,21 @@ def gameloop():
                 pygame.quit()
                 sys.exit()
 
-        if stisknuto[pygame.K_ESCAPE]:
-            pygame.QUIT
-            sys.exit()
-
         #ovládání auta
-        if udalost.type == pygame.KEYDOWN:
-            if udalost.key == pygame.K_RIGHT:
-                rychlostX = 8
-            if udalost.key == pygame.K_LEFT:
-                rychlostX = -8
+            if udalost.type == pygame.KEYDOWN:
+                if udalost.key == pygame.K_RIGHT:
+                    rychlostX = 8
+                if udalost.key == pygame.K_LEFT:
+                    rychlostX = -8
             
         #    if udalost.key == pygame.K_UP:
         #         rychlostY = -5
         #    if udalost.key == pygame.K_DOWN:
         #         rychlostY = 5
                  
-        if udalost.type == pygame.KEYUP:
-            if udalost.key == pygame.K_LEFT or udalost.key == pygame.K_RIGHT:
-                rychlostX = 0
+            if udalost.type == pygame.KEYUP:
+                if udalost.key == pygame.K_LEFT or udalost.key == pygame.K_RIGHT:
+                    rychlostX = 0
                  
         #if udalost.type == pygame.KEYUP:
         #    if udalost.key == pygame.K_UP or udalost.key == pygame.K_DOWN:
@@ -189,7 +281,7 @@ def gameloop():
             
         if stisknuto[pygame.K_SPACE]:
             autoX = 440
-            autoY = 480
+            autoY = 460
 
         pozadi()
 
@@ -209,7 +301,7 @@ def gameloop():
             crash(autoX, autoY)
             
         if autoX < 100:
-           crash(autoX, autoY)
+            crash(autoX, autoY)
            
         if autoY < prekazka_starty + prek_vyska:
             if autoX >= prekazka_startx and autoX <= prekazka_startx+prek_sirka:
@@ -218,7 +310,8 @@ def gameloop():
                     crash(autoX,autoY-vyska/2)        
            
         auto(autoX, autoY)
-        prekazky(prekazka_startx,prekazka_starty,fotoauta2)
+        prekazky(prekazka_startx,prekazka_starty,fotoauta3)
+        prekazky(prekazka_startx,prekazka_starty,fotoauta3)
         
         highscore(pocet)              
         pocet+=1
@@ -228,9 +321,14 @@ def gameloop():
             prekazka_startx = random.randrange(400,700-sirka)
             prekazka_starty = -200
         
+        if prekazka_starty > sirka_okna:
+            prekazka_startx = random.randrange(100,300)
+            prekazka_starty = -200
+        
         pygame.display.update()
         hodiny.tick(60)
         
+mainmenu()        
 gameloop()
 pygame.quit()
 quit()
